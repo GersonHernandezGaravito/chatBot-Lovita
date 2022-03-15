165c9fc50a705fba589e2dc1e8e92680950cbb71
Gerson Hernández <ghernandezg13@miumg.edu.gt>
2022-03-14 21:02:46 -0600

    first comit

--- app.py
+++ app.py
@@ -1,99 +1,180 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from __future__ import print_function
+#Para crear el ambiente virtual
+#python3 -m venv .venv
+#source .venv/bin/activate
 
-from flask import Flask, redirect, url_for, render_template, session, request
-from flask_socketio import emit, SocketIO
+from __future__ import print_function
+from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
+#python3.7 -m pip install werkzeug
+from werkzeug.debug import DebuggedApplication
+from flask import Flask, render_template, request
+import json
+
+#python3.7 -m pip install gevent
+from gevent import monkey
+
+#db and aiml imports
+#python3.7 -m pip3.7 install tinydb
 from tinydb import TinyDB, Query
-import argparse
 import os
+#python3.7 -m pip install python-aiml
 import aiml
 
+monkey.patch_all()
 
-# Carga aplicación Flask
-app = Flask(__name__)
+# Para la interfaz y otras cosas
+flask_app = Flask(__name__)
+flask_app.debug = True
 
-# Habilita sockets in aplicación Flaks
-socketio = SocketIO(app)
-
-# Carga nucleo de AIML
 k = aiml.Kernel()
+BRAIN_FILE = "brain.dump"
+if os.path.exists(BRAIN_FILE):
+    print("Cargando desde archivo cerebral file: " + BRAIN_FILE)
+    k.loadBrain(BRAIN_FILE)
+else:
+    print("Parsing aiml files")
+    k.bootstrap(learnFiles="std-startup.aiml", commands="load aiml b")
+    print("Cargando Skynet: " + BRAIN_FILE)
+    k.saveBrain(BRAIN_FILE)
 
-# Carga base de datos de conversaciones
+
 db = TinyDB('conversations.json')
-Usuario= Query()
+Usuario = Query()
 
-# Página principa.
-@app.route('/')
-def login():
-  return render_template('home.html')
 
-# Paǵina acerca de
-@app.route('/about')
-def about():
-  return render_template('about.html')
+#INICIO GERSON
+#librerías para conexion a facebook
+from pymessenger.bot import Bot
 
-# Página con chat
-@app.route('/chat')
-def chat():
-  username=request.args['username']
-  if not len(username)>0:
-      username='desconocido'
-  user=db.search(Usuario.user == username)
-  print(user)
-  if len(user)==0:
-      user=db.insert({'user':username,'conversations':[[]]})
-      user=db.get(eid=user)
-  else:
-      user=user[0]
-      conv=user['conversations']
-      conv.append([])
-      db.update({'conversations':conv},eids=[user.eid])
+#Token para permitir acceso a la pagina
+ACCESS_TOKEN = 'EAAFZBSupvax8BAKZCEOyv3sEjOWlkhOC802XZCVRj2SiaezAIzbJHfieA6BecrPVcoaK3UeFgckd6FZCm5856kgbqDRK9BWjEvCGAvKWZAsJx421Qser2HVkrtUTAcHVpvPnjuEeACvDYYTqV2Kx6TOHjiOVc82S4DGOFIQAVgwZDZD'
+VERIFY_TOKEN = 'PRUEBATOKEN'
+bot = Bot(ACCESS_TOKEN)
 
-  return render_template('chat.html',username=username)
+#class ChatApplication(WebSocketApplication):
 
-# Mensajes con chat
-@socketio.on('message', namespace='/ask')
-def receive_message(message):
-    username,message_=message['data'].split(':',1)
-    user=db.search(Usuario.user == username)
-    if len(user)==0:
-      user=db.insert({'user':username,'conversations':[[]]})
-      user=db.get(eid=user)
+
+
+@flask_app.route("/", methods=['GET', 'POST'])
+def receive_message():
+    if request.method == 'GET':
+        token_enviado = request.args.get("hub.verify_token")
+        return verificar_token(token_enviado)
     else:
-      user=user[0]
+        # Obtener el mensaje
+       output = request.get_json()
+       contador_mensajes = 0
+       #print (output)
+       for event in output['entry']:
+          messaging = event['messaging']
+          for message in messaging:
+            if message.get('message'):
+                contador_mensajes = contador_mensajes + 1
+                #Guarda el ID del usuario para responder
+                id_usuario = message['sender']['id']
+                #Si es primer mensaje, guarda el id del usuario
+                #if contador_mensajes == 0:
+                    #send_client_list(id_usuario)
+                #Si llega un mensaje lo envía a proceso de respuesta
+                if message['message'].get('text'):
+                    mensaje_recibido = message['message'].get('text')
+                    broadcast(id_usuario, mensaje_recibido)
+                    #mensaje_respuesta(id_usuario, mensaje_recibido)
+    return "Mensaje Recibido"
+#FIN GERSON
+
+def send_client_list(id_usuario):
+    #current_client = self.ws.handler.active_client
+    #current_client.nickname = message['nickname']
+ 
+    #username = current_client.nickname
+    username = id_usuario
+    if not len(username)>0:
+        username='desconocido'
+    user=db.search(Usuario.user == username)
+    #print(user)
+        
+    if len(user) == 0:
+        user = db.insert({'user': username, 'conversations': [[]]})
+        #user = db.get(eid=user)
+    else:
+        user=user[0]
+        conv=user['conversations']
+        if not conv:
+            print(conv)
+        #conv.append([])
+          
+        #if not conv:
+            
+        #db.update({'conversations':conv},doc_ids=[user.doc_id])
+
+    #self.ws.send(json.dumps({
+    #    'msg_type': 'update_clients',
+    #    'clients': [
+    #        getattr(client, 'nickname', 'anonymous')
+    #        for client in self.ws.handler.server.clients.values()
+    #    ]
+    #}))
+
+
+def broadcast(id_user, message_):
+    #nickname = message['nickname']
+    #message_ = message['message']
+    #print (bot.get_user_info(nickname))
+    profile = bot.get_user_info(id_user)
+    first_name = profile.get('first_name')
+    last_name = profile.get('last_name')
+    
+    user=db.search(Usuario.id_user == id_user)
+    if len(user)==0:
+        user=db.insert({'id_user':id_user, 'first_name':first_name, 'last_name':last_name,'conversations':[[]]})
+        user=db.get(eid=id_user)
+    else:
+        user=user[0]
     ans=k.respond(message_)
+    
+    if message_.upper() == "HOLA":
+        ans = ans + " " + first_name
+    
     conv=user['conversations']
+    
     conv[-1].append({'msg':message_,'ans':ans})
-    db.update({'conversations':conv},eids=[user.eid])
-    emit('response', {'data': ans.lower()})
+    
+    db.update({'conversations':conv},doc_ids=[user.doc_id])
+    #emit('response', {'data': ans.lower()})
 
+#INCIO GERSON
+    #Llama a metodo para enviar mensaje
+    mensaje_respuesta(id_user, ans, message_, first_name)
+#FIN GERSON
+    
+    #for client in self.ws.handler.server.clients.values():
+    #    client.ws.send(json.dumps({
+    #        'msg_type': 'message',
+    #        'nickname': message['nickname'],
+    #        'message': message['message']
+    #    }))
+    #for client in self.ws.handler.server.clients.values():
+    #    client.ws.send(json.dumps({
+    #        'msg_type': 'botanswer',
+    #        'nickname': 'CHATBOT',
+    #        'message': ans
+    #    }))
+    
+#INICIO GERSON
+def verificar_token(token_enviado):
+    #take token sent by facebook and verify it matches the verify token you sent
+    #if they match, allow the request, else return an error 
+    if token_enviado == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Token Invalido' 
 
-# Función principal (interfaz con línea de comandos)
-if __name__ == '__main__':
-    p = argparse.ArgumentParser("pyAIML")
-    p.add_argument("--host",default="127.0.0.1",
-            action="store", dest="host",
-            help="Root url [127.0.0.1]")
-    p.add_argument("--port",default=5000,type=int,
-            action="store", dest="port",
-            help="Port url [500]")
-    p.add_argument("--aiml",default="aiml/mibot.aiml",type=str,
-            action="store", dest="aiml",
-            help="AIML file with rules [aiml/mibot.aiml]")
-    p.add_argument("--debug",default=False,
-            action="store_true", dest="debug",
-            help="Use debug deployment [Flase]")
-    p.add_argument("-v", "--verbose",
-            action="store_true", dest="verbose",
-            help="Verbose mode [Off]")
+#Se usa la librería PyMessenger para responder al usuario que envió el mensaje
+def mensaje_respuesta(id_usuario, response, message, name):
+    #Envia mensaje (id del usuario al que envía, texto a enviar)
+    
+    bot.send_text_message(id_usuario, response)
+    return "enviado"
 
-    opts = p.parse_args()
-
-    k.learn(opts.aiml)
-    k.respond("load aiml b")
-
-    socketio.run(app,
-	    debug=opts.debug,
-            host=opts.host,
-            port=opts.port)
+if __name__ == "__main__":
+    flask_app.run()
+#FIN GERSON
